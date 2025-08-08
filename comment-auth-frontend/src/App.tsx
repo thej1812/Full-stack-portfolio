15c267da91f976cf8c53ed89022da68d467f245d
thej1812 <thejashwini1800@gmail.com>
2025-08-08 22:37:15 +0530

    app.jsx

--- comment-auth-frontend/src/App.tsx
+++ comment-auth-frontend/src/App.tsx
@@ -1,10 +1,13 @@
-import Home from './pages/home';
-import './App.css'
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
+import Login from "./pages/login";
+
 function App() {
   return (
-    <div>
-      <Home />
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
   );
 }
 
