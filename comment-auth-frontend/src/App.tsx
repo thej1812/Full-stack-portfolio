aef3922df14e4d11b4942845077938e2f33105b9
thej1812 <thejashwini1800@gmail.com>
2025-08-10 20:14:11 +0530

    app

--- comment-auth-frontend/src/App.tsx
+++ comment-auth-frontend/src/App.tsx
@@ -1,12 +1,18 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 import Login from "./pages/login";
+import Signup from "./pages/signup";
+import AboutMe from "./pages/AboutMe"; // <-- Import your About Me page
+import Comments from "./pages/Comments";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/about" element={<AboutMe />} />
+      <Route path="/comments" element={<Comments/>}/>
     </Routes>
   );
 }
