2566c0756c88d78f48edbd2a9f434ac93124f517
thej1812 <thejashwini1800@gmail.com>
2026-01-09 23:47:49 +0530

    comment

--- comment-auth-frontend/src/pages/Comments.tsx
+++ comment-auth-frontend/src/pages/Comments.tsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { getToken } from '../utils/auth';
 import { useNavigate, Link } from 'react-router-dom';
 import './comments.css';
-import Particles from '@/assets/Particles/Particles';
+import Particles from '../assets/Particles/Particles';
 
 // ✅ API base URL from Vite environment
 const API_URL = import.meta.env.VITE_API_URL;
