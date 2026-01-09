4ad46b6c723bd29da3f2e781d06c9cdd426dd8ac
thej1812 <thejashwini1800@gmail.com>
2026-01-09 23:18:51 +0530

    app

--- comment-auth-frontend/src/App.tsx
+++ comment-auth-frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import Login from "./pages/login";
 import Signup from "./pages/signup";
 import AboutMe from "./pages/AboutMe"; // <-- Import your About Me page
