ad15d362fe4ade4402f02aac7129638cf91c3bea
thej1812 <thejashwini1800@gmail.com>
2026-01-11 19:21:29 +0530

    Login

--- comment-auth-frontend/src/pages/Login.tsx
+++ comment-auth-frontend/src/pages/Login.tsx
@@ -13,7 +13,7 @@ export default function Login() {
 
   const handleLogin = async () => {
     try {
-      const res = await axios.post('http://localhost:5000/api/auth/login', {
+      const res = await axios.post('https://full-stack-portfolio-jdcz.onrender.com/api/auth/login', {
         username,
         password
       });
